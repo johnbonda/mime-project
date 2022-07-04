9fb676543c781805ae3a4ea00f0e824f368fd4ff
John <john.bonda7@gmail.com>
2022-07-04 22:52:26 +0530

    initial commit

---
+++ src/main/java/com/kp/mime/mimeproject/MimeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kp.mime.mimeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kp/mime/mimeproject/MimeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.mime.mimeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
