85081603f28eebb9f8236e91cd7cbaddf3b8de46
John <john.bonda7@gmail.com>
2022-07-08 20:15:31 +0530

    html done

--- src/main/java/com/kp/mime/mimeproject/controllers/HomeController.java
+++ src/main/java/com/kp/mime/mimeproject/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.kp.mime.mimeproject.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import com.kp.mime.mimeproject.repositories.RoleRepository;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin
 public class HomeController {
 
     @Autowired
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,3 +1,5 @@
+const state = {}
+
 // document.getElementById("myName").setAttribute('value', 'Hello world') 
 window.onload = () => {
     // const queryString = window.location.search;
@@ -7,7 +9,8 @@ window.onload = () => {
     // get mimename from url
     // call server and get mime data with name
     // populate data
-    document.getElementById("firstName").value = "Hello"
+    // document.getElementById("firstName").value = "Hello"
+    getInitialData()
 
     
 }
@@ -21,6 +24,88 @@ var phoneBlurHandler = () => {
         "faxPhone",
     ]
     phoneTypes.forEach(type => {
-        document.getElementById(type).disabled = !document.getElementById(type+'Type').checked
+        var checked = document.getElementById(type+'Type').checked
+        text = document.getElementById(type)
+        text.disabled = !checked
+        if(checked) {
+            text.focus()
+        }
+    })
+}
+
+var getInitialData = () => {
+    fetch("http://localhost:8080/api/roles")
+    .then(response => {
+        if(!response.ok) {
+            alert("Unable to fetch roles")
+        }
+        else {
+            return response.json()
+        }
+    })
+    .then(data => {
+        console.log(data)
+        createBoxes("roles", data)
+    });
+    fetch("http://localhost:8080/api/orgs")
+    .then(response => {
+        if(!response.ok) {
+            alert("Unable to fetch orgs")
+        }
+        else {
+            return response.json()
+        }
+    })
+    .then(data => {
+        console.log(data)
+        createBoxes("orgs", data)
+    });
+    fetch("http://localhost:8080/api/events")
+    .then(response => {
+        if(!response.ok) {
+            alert("Unable to fetch events")
+        }
+        else {
+            return response.json()
+        }
+    })
+    .then(data => {
+        console.log(data)
+        createBoxes("events", data)
+    });
+    fetch("http://localhost:8080/api/issues")
+    .then(response => {
+        if(!response.ok) {
+            alert("Unable to fetch issues")
+        }
+        else {
+            return response.json()
+        }
+    })
+    .then(data => {
+        console.log(data)
+        createBoxes("issues", data)
+    });
+}
+
+
+var createBoxes = (type, data) => {
+    data.forEach(dat => {
+        const checkboxId = type + '-' + dat.id
+        const checkbox = document.createElement("input")
+        checkbox.setAttribute('type', 'checkbox')
+        checkbox.setAttribute('name', 'roles')
+        checkbox.setAttribute('id', checkboxId)
+
+        const label = document.createElement("label")
+        label.setAttribute('for', checkboxId)
+        label.innerHTML = dat.name
+
+        const item = document.createElement("span")
+        item.className = "item ditem"
+        item.appendChild(checkbox)
+        item.appendChild(label)
+
+        document.getElementById(type + "-field").appendChild(item)
     })
 }
